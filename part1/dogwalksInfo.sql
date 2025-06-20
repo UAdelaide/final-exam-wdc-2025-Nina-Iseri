f7cdee1b558bcab78d0b5826ede222ec4bbe47bd
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 16:18:10 +0000

    [automatic commit] 2025-06-20 16:18:10 UTC 203.222.144.123

--- part1/dogwalksInfo.sql
+++ part1/dogwalksInfo.sql
@@ -2,4 +2,5 @@ INSERT INTO Users
 VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-('carol', 'carol@example.com', 'hashed789', )
+('carol', 'carol@example.com', 'hashed789', 'owner'),
+('david', 'david')
