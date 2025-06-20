6950d9e00bd3359beb68c92cc52a99e8c96f6a2b
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 23:27:57 +0000

    [automatic commit] 2025-06-20 23:27:57 UTC 203.222.144.123

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -52,6 +52,9 @@ router.post('/login', async(req, res) => {
     res.cookie('username', rows[0].username);
     res.cookie('role', rows[0].role);
     req.session.user_id = rows[0].user_id;
+
+
+    
     // Redirect to page according to role
     res.redirect('https://localhost:8080/owner-dashboard.html');
 
