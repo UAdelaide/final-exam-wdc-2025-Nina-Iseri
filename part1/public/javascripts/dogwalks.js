f850e4e9ab7bbc601ccc8668da4ed996c4abb3a2
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 19:57:06 +0000

    [automatic commit] 2025-06-20 19:57:06 UTC 203.222.144.123

--- part1/public/javascripts/dogwalks.js
+++ part1/public/javascripts/dogwalks.js
@@ -3,10 +3,10 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            dog_of_the_day: []
+            
         };
     },
     methods: {
-        
+
     }
 }).mount("body");
