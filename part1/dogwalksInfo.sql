a920665d77e6b63de2068e06fde803614f4f9646
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 16:18:51 +0000

    [automatic commit] 2025-06-20 16:18:51 UTC 203.222.144.123

--- part1/dogwalksInfo.sql
+++ part1/dogwalksInfo.sql
@@ -3,4 +3,5 @@ VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol', 'carol@example.com', 'hashed789', 'owner'),
-('david', 'david@example.com', 'hashed10', '')
+('david', 'david@example.com', 'hashed10', 'walker'),
+('Alan', )
