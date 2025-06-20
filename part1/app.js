afb2562640eb7cf7373b8a471e83909044d4cbc5
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 17:24:53 +0000

    [automatic commit] 2025-06-20 17:24:53 UTC 203.222.144.123

--- part1/app.js
+++ part1/app.js
@@ -23,7 +23,7 @@ let db;
       const connection = await mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: '' // Set your MySQL root password
+        password: '' // No pass
       });
 
       // Create the database if it doesn't exist
