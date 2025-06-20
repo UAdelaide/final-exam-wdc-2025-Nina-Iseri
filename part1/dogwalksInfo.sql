ffbbe149ad15e87b4517580cf17ed25ba58f639a
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 17:02:25 +0000

    [automatic commit] 2025-06-20 17:02:25 UTC 203.222.144.123

--- part1/dogwalksInfo.sql
+++ part1/dogwalksInfo.sql
@@ -19,6 +19,7 @@ SELECT dog_id, '2025-06-10 08:00:00', '30', 'Parklands', 'open' FROM Dogs WHERE 
 SELECT dog_id, '2025-06-10 09:30:00', '45', 'Beachside Ave', 'accepted' FROM Dogs WHERE name = 'Bella' UNION ALL
 SELECT dog_id, '1970-01-01 00:00:00', '5', 'Moon', 'open' FROM Dogs WHERE name = 'Emma' UNION ALL
 SELECT dog_id, '2025-06-11 10:00:00', '60', 'Rundle St', 'accepted' FROM Dogs WHERE name = 'Frank' UNION ALL
+SELECT dog_id, '2025-06-12 '
 
 
 
