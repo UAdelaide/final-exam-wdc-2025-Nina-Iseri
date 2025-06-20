a31355d1d7915bf9f94e78263173fac8f3d64105
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 20:02:27 +0000

    [automatic commit] 2025-06-20 20:02:27 UTC 203.222.144.123

--- part1/public/javascripts/dogwalks.js
+++ part1/public/javascripts/dogwalks.js
@@ -16,8 +16,10 @@ createApp({
             await fetch("https://dog.ceo/api/breeds/image/random")
             .then(res => res.json)
             .then(data => {
-                this.dog_of_the_day.imageUrl = data.
+                this.dog_of_the_day.imageUrl = data.message;
             });
+        } catch (err) {
+            
         }
     }
 }).mount("body");
