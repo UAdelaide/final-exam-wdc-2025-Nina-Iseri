6982c506861da0c24edca74a4eededa91c567dd4
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 16:53:02 +0000

    [automatic commit] 2025-06-20 16:53:02 UTC 203.222.144.123

--- part1/dogwalksInfo.sql
+++ part1/dogwalksInfo.sql
@@ -14,3 +14,4 @@ SELECT user_id, 'Emma', 'large' FROM Users Where username = 'bobwalker' UNION AL
 SELECT user_id, 'Frank', 'medium' FROM Users Where username = 'david' UNION ALL
 SELECT user_id, 'George', 'small' FROM Users Where username = 'alice123';
 
+INSERT INTO WalkRequests ()
