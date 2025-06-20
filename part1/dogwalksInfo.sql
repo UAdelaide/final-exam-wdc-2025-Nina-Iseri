4381dcb3b91d9e8a623d5c7d56619ba4989f3f01
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 18:48:22 +0000

    [automatic commit] 2025-06-20 18:48:22 UTC 203.222.144.123

--- part1/dogwalksInfo.sql
+++ part1/dogwalksInfo.sql
@@ -22,6 +22,13 @@ SELECT dog_id, '1970-01-01 00:00:00', '5', 'Moon', 'open' FROM Dogs WHERE name =
 SELECT dog_id, '2025-06-11 10:00:00', '60', 'Rundle St', 'accepted' FROM Dogs WHERE name = 'Frank' UNION ALL
 SELECT dog_id, '2025-06-12 12:00:00', '15', 'Hub Central', 'cancelled' FROM Dogs WHERE name = 'Max';
 
+INSERT INTO WalkApplications (request_id, walker_id, status)
+VALUES
+(4, 2, 'accepted'),
+(5, 2, 'accepted');
+
+
+
 SELECT username AS walker_username, total_ratings, average_rating, completed_walks
     FROM (
         SELECT username, SUM(rating) AS total_ratings, SUM(rating) / COUNT(rating) AS average_rating
