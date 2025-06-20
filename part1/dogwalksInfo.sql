eee01015cbfa2aa16a782bb9500bd328597d970e
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 18:50:03 +0000

    [automatic commit] 2025-06-20 18:50:03 UTC 203.222.144.123

--- part1/dogwalksInfo.sql
+++ part1/dogwalksInfo.sql
@@ -15,13 +15,13 @@ SELECT user_id, 'Emma', 'large' FROM Users Where username = 'bobwalker' UNION AL
 SELECT user_id, 'Frank', 'medium' FROM Users Where username = 'david' UNION ALL
 SELECT user_id, 'George', 'small' FROM Users Where username = 'alice123';
 
+
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 SELECT dog_id, '2025-06-10 08:00:00', '30', 'Parklands', 'open' FROM Dogs WHERE name = 'Max' UNION ALL
 SELECT dog_id, '2025-06-10 09:30:00', '45', 'Beachside Ave', 'accepted' FROM Dogs WHERE name = 'Bella' UNION ALL
 SELECT dog_id, '1970-01-01 00:00:00', '5', 'Moon', 'open' FROM Dogs WHERE name = 'Emma' UNION ALL
-SELECT dog_id, '2025-06-11 10:00:00', '60', 'Rundle St', 'accepted' FROM Dogs WHERE name = 'Frank' UNION ALL
-SELECT dog_id, '2025-06-12 12:00:00', '15', 'Hub Central', 'cancelled' FROM Dogs WHERE name = 'Max';
-
+SELECT dog_id, '2025-06-11 10:00:00', '60', 'Rundle St', 'completed' FROM Dogs WHERE name = 'Frank' UNION ALL
+SELECT dog_id, '2025-06-12 12:00:00', '15', 'Hub Central', 'completed' FROM Dogs WHERE name = 'Max'
 INSERT INTO WalkApplications (request_id, walker_id, status)
 VALUES
 (4, 2, 'accepted'),
