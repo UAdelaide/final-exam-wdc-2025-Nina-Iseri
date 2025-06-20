2a17573ac7b444b50382a6dfcb452766d25a051a
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 22:45:43 +0000

    [automatic commit] 2025-06-20 22:45:43 UTC 203.222.144.123

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -44,7 +44,7 @@ router.post('/login', async(req, res) => {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
       WHERE username = ? AND password_hash = ?
-    `, [user.user, user.pass]);
+    `, [user, pass]);
     if (rows.length === 0) {
       console.log("not found");
       return res.status(401).json({error: 'Invalid credentials'});
