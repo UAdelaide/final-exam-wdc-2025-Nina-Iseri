8d1a27a732627ff1b1435bd30c9ab28f44d64764
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 18:05:47 +0000

    [automatic commit] 2025-06-20 18:05:47 UTC 203.222.144.123

--- part1/app.js
+++ part1/app.js
@@ -151,7 +151,7 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/:status', async (req, res) => {
     let status = req.params.status || '';
     if (status != 'open') {
-        res.status(400).json({ error: 'Invalid walkrequest status'});
+        return res.status(400).json({ error: 'Invalid walkrequest status'});
         return;
     }
     try {
