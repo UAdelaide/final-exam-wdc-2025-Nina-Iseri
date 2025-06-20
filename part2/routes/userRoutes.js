550bf0fe517d90a1fd69babe880178669b512230
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 23:56:07 +0000

    [automatic commit] 2025-06-20 23:56:07 UTC 203.222.144.123

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -58,7 +58,7 @@ router.post('/login', async(req, res) => {
     req.session.save();
     // console.log(req.session);/
 
-    res.d({ message: 'Login successful', user: rows[0].username });
+    res.send(rows[0].username);
   } catch (err) {
     res.status(500).json({error: 'Login failed'});
   }
