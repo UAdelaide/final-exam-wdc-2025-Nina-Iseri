c7fbd22f4f4f3f3abd6182d0bea6f3f201101442
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 16:46:00 +0000

    [automatic commit] 2025-06-20 16:46:00 UTC 203.222.144.123

--- part1/dogwalksInfo.sql
+++ part1/dogwalksInfo.sql
@@ -8,8 +8,8 @@ VALUES
 
 
 INSERT INTO Dogs (owner_id, name, size)
-SELECT user_id, 'Max', 'medium' FROM Users Where username = 'alice123',
-SELECT user_id, 'Bella', 'small' FROM Users Where username = 'carol123',
-SELECT user_id, 'Emma', 'large' FROM Users Where username = 'bobwalker',
-SELECT user_id, 'Frank', 'medium' FROM Users Where username = 'david',
+SELECT user_id, 'Max', 'medium' FROM Users Where username = 'alice123' UNION ALL
+SELECT user_id, 'Bella', 'small' FROM Users Where username = 'carol123' UNION ALL
+SELECT user_id, 'Emma', 'large' FROM Users Where username = 'bobwalker' UNION ALL
+SELECT user_id, 'Frank', 'medium' FROM Users Where username = 'david' UNION ALL
 SELECT user_id, 'George', 'small' FROM Users Where username = 'alice123';
