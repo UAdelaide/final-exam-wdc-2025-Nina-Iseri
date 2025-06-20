d728e017d54366661fa4d5a142a5421671430d4b
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 19:13:30 +0000

    [automatic commit] 2025-06-20 19:13:30 UTC 203.222.144.123

--- part1/dogwalksInfo.sql
+++ part1/dogwalksInfo.sql
@@ -45,7 +45,7 @@ SELECT username AS walker_username, total_ratings, average_rating, completed_wal
         GROUP BY username
     ) sub1
     INNER JOIN (
-        SELECT username, IFNULL(completed_walks, 0)
+        SELECT username, IFNULL(completed_walks, 0) AS completed_walks
         FROM (
             SELECT walker_id, COUNT(*) AS completed_walks
             FROM WalkRequests
