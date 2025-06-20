80bd9d3b2221b24a45d02392be5a889d24209efb
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 18:40:19 +0000

    [automatic commit] 2025-06-20 18:40:19 UTC 203.222.144.123

--- part1/app.js
+++ part1/app.js
@@ -137,8 +137,8 @@ let db;
       await db.execute(`
         INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating)
         VALUES
-        (1, 2, 1, 5),
-        (3, 2, 1, 4)
+        (4, 2, 1, 5),
+        (5, 2, 1, 4)
       `);
     } catch (err) {
       console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
