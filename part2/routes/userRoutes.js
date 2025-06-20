8fa6dbb93ad3985a81a49d73e684097f4f903536
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 22:21:55 +0000

    [automatic commit] 2025-06-20 22:21:55 UTC 203.222.144.123

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -43,7 +43,8 @@ router.post('/login', async(req, res) => {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
       WHERE username = ? AND password_hash = ?
-      `, [username, password])
+      `, [username, password]);
+      
   }
 });
 
