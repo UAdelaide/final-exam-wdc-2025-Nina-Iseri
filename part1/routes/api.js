bc82ae3b9080bc89fc12283f85b2bdf2e3ec5b18
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 19:40:00 +0000

    [automatic commit] 2025-06-20 19:40:00 UTC 203.222.144.123

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -6,16 +6,4 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/dogs', function(req, res, next) {
-
-});
-
-router.get('/walkrequests:status', function(req, res, next) {
-
-});
-
-router.get('/walkers/summary', function(req, res, next) {
-
-});
-
 module.exports = router;
