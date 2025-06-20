8a818f1c733429022b8156c18e995d838cd1c3ea
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 19:07:28 +0000

    [automatic commit] 2025-06-20 19:07:28 UTC 203.222.144.123

--- part1/dogwalksInfo.sql
+++ part1/dogwalksInfo.sql
@@ -49,7 +49,7 @@ SELECT username AS walker_username, total_ratings, average_rating, completed_wal
         FROM (
             SELECT walker_id, COUNT(*) AS completed_walks
             FROM WalkRequests
-            INNER JOIN 
+            INNER JOIN WalkApplications ON WalkApplications.request_id = WalkRequests.
         ) sub
         RIGHT OUTER JOIN Users ON Users.user_id = sub.walker_id
         WHERE role = 'walker'
