63247ef6733ff2818a202adf9710ce149c7f97d2
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-20 22:40:41 +0000

    [automatic commit] 2025-06-20 22:40:41 UTC 203.222.144.123

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -189,13 +189,13 @@ function login(){
         if (this.readyState == 4 && this.status == 200) {
             alert("Welcome "+this.responseText);
         } else if (this.readyState == 4 && this.status >= 400) {
-            alert(this.responseText);
+            alert("Login failed");
         }
     };
 
     // Open connection to server & send the post data using a POST request
     // We will cover POST requests in more detail in week 8
-    xmlhttp.open("POST", "/users/login", true);
+    xmlhttp.open("POST", "/api/users/login", true);
     xmlhttp.setRequestHeader("Content-type", "application/json");
     xmlhttp.send(JSON.stringify(user));
 
