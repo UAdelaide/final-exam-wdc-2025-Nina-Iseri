4e04811125ebff3f3410af421156d5a310199d87
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-21 02:25:17 +0000

    [automatic commit] 2025-06-21 02:25:17 UTC 203.222.144.123

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -187,7 +187,7 @@ async function login() {
     xmlhttp.onreadystatechange = async function() {
         if (this.readyState == 4 && this.status == 200) {
             // let res = this.response.user;
-            await fetch('localhost:8080/api/users/redirect');
+            
         } else if (this.readyState == 4 && this.status >= 400) {
             alert("Login failed");
         }
