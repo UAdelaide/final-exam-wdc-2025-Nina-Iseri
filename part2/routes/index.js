433c2b88a4184f68f065317421d0f045bd592a17
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-21 00:27:18 +0000

    [automatic commit] 2025-06-21 00:27:18 UTC 203.222.144.123

--- part2/routes/index.js
+++ part2/routes/index.js
@@ -4,7 +4,9 @@ const router = express.Router();
 // GET home page
 router.get('/', (req, res, next) => {
     let user_id = req.session.user_id || '';
-    if (user_id.)
+    if (!user_id.length) {
+        res
+    }
 });
 
 module.exports = router;
