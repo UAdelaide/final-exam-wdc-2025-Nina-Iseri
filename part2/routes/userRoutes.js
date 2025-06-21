02aeaa3ea39dfe02193f697faea05ca6a8432ac6
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-21 02:35:41 +0000

    [automatic commit] 2025-06-21 02:35:41 UTC 203.222.144.123

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -73,7 +73,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
-router.get('/')
+router.get('/owner-dashboard', )
 
 router.get('/redirect', async(req, res) => {
   console.log('redirecting');
