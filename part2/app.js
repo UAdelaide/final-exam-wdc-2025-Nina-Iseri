48675a2732939b92555d198508cc48713b3e3536
Nina-Iseri <a1918267@adelaide.edu.au>
2025-06-21 01:17:36 +0000

    [automatic commit] 2025-06-21 01:17:36 UTC 203.222.144.123

--- part2/app.js
+++ part2/app.js
@@ -20,9 +20,9 @@ const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 const indexRoutes = require('./routes/index');
 
+app.use('/', indexRoutes);
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-app.use('/', indexRoutes);
 
 // Export the app instead of listening here
 module.exports = app;
